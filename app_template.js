ac013c34dfe9b5bd6fa51c6e484789e87e0ea1ff
Kitiyaparnnn <parn.takhum@gmail.com>
2021-01-12 13:57:23 +0700

    ;-;

--- app_template.js
+++ app_template.js
@@ -1,1 +1,13 @@
 //your code here!
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#button-compute').click = function () {
+        const num = document.createElement('p')
+        const number = document.querySelector("#input-number").value
+       
+        console.log(number)
+        num.innerHTML = '620610774' + number
+        num.append()
+
+    }
+    
+},false)
